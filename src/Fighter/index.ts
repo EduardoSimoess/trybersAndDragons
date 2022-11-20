fc2ae29e08bb13df04a2f5b564b507913d1e835c
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-11-20 15:12:07 -0300

    Quesito 10

--- src/Fighter/index.ts
+++ src/Fighter/index.ts
@@ -1,3 +1,7 @@
 import SimpleFighter from './SimpleFighter';
+import Fighter from './Fighter';
 
-export default SimpleFighter;
+// export { Fighter, SimpleFighter };
+export default Fighter;
+
+export { SimpleFighter };
