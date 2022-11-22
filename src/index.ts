faeb60bba7d983c4d99e474a342ff5b6cafd9377
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-11-22 13:34:46 -0300

    finalizando

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Monster from './Monster';
+
+const player1 = new Character('Edu');
+const player2 = new Character('Hunter seang jin woo');
+const player3 = new Character('Thomas Andrey');
+
+player1.levelUp();
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Monster();
+
+const pvp = new PVP(player1, player2);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export { player1, player2, player3, monster1, monster2, pvp, pve, runBattles };
